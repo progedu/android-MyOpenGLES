fdf68b3340104adf222d6eb6d803f33286b507f3
Soichiro-Yoshimura <yoshimura@soichiro.org>
2016-12-12 18:21:20 +0900

    Define a Projection

--- app/src/main/java/com/example/myopengles/MyGLRenderer.java
+++ app/src/main/java/com/example/myopengles/MyGLRenderer.java
@@ -2,6 +2,7 @@ package com.example.myopengles;
 
 import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
+import android.opengl.Matrix;
 
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
@@ -29,8 +30,20 @@ public class MyGLRenderer implements GLSurfaceView.Renderer {
         mTriangle.draw();
     }
 
+    // mMVPMatrix is an abbreviation for "Model View Projection Matrix"
+    private final float[] mMVPMatrix = new float[16];
+    private final float[] mProjectionMatrix = new float[16];
+    private final float[] mViewMatrix = new float[16];
+
+    @Override
     public void onSurfaceChanged(GL10 unused, int width, int height) {
         GLES20.glViewport(0, 0, width, height);
+
+        float ratio = (float) width / height;
+
+        // this projection matrix is applied to object coordinates
+        // in the onDrawFrame() method
+        Matrix.frustumM(mProjectionMatrix, 0, -ratio, ratio, -1, 1, 3, 7);
     }
 
     public static int loadShader(int type, String shaderCode){
