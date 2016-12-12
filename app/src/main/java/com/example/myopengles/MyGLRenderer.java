599a864a0718197e0d5eadac4dad28332b51572f
Soichiro-Yoshimura <yoshimura@soichiro.org>
2016-12-12 17:34:15 +0900

    Add loadShader

--- app/src/main/java/com/example/myopengles/MyGLRenderer.java
+++ app/src/main/java/com/example/myopengles/MyGLRenderer.java
@@ -30,4 +30,17 @@ public class MyGLRenderer implements GLSurfaceView.Renderer {
     public void onSurfaceChanged(GL10 unused, int width, int height) {
         GLES20.glViewport(0, 0, width, height);
     }
+
+    public static int loadShader(int type, String shaderCode){
+
+        // create a vertex shader type (GLES20.GL_VERTEX_SHADER)
+        // or a fragment shader type (GLES20.GL_FRAGMENT_SHADER)
+        int shader = GLES20.glCreateShader(type);
+
+        // add the source code to the shader and compile it
+        GLES20.glShaderSource(shader, shaderCode);
+        GLES20.glCompileShader(shader);
+
+        return shader;
+    }
 }
