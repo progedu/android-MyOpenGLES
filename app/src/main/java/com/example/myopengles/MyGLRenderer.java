4da0960713511226ea3876060a31513816835368
Soichiro-Yoshimura <yoshimura@soichiro.org>
2016-12-12 18:39:44 +0900

    Applying Projection and Camera Views

--- app/src/main/java/com/example/myopengles/MyGLRenderer.java
+++ app/src/main/java/com/example/myopengles/MyGLRenderer.java
@@ -27,7 +27,14 @@ public class MyGLRenderer implements GLSurfaceView.Renderer {
         // Redraw background color
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
 
-        mTriangle.draw();
+        // Set the camera position (View matrix)
+        Matrix.setLookAtM(mViewMatrix, 0, 0, 0, -3, 0f, 0f, 0f, 0f, 1.0f, 0.0f);
+
+        // Calculate the projection and view transformation
+        Matrix.multiplyMM(mMVPMatrix, 0, mProjectionMatrix, 0, mViewMatrix, 0);
+
+        // Draw shape
+        mTriangle.draw(mMVPMatrix);
     }
 
     // mMVPMatrix is an abbreviation for "Model View Projection Matrix"
